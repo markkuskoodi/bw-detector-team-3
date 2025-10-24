9278e8e28ab7bc7c53d306aac9b45f1e9bedcadd
Andreasmalleus <andreasmalleus@gmail.com>
2025-10-24 15:03:03 +0300

    person validator fix + git ignore

--- src/main/java/ee/digit25/detector/domain/person/PersonValidator.java
+++ src/main/java/ee/digit25/detector/domain/person/PersonValidator.java
@@ -1,6 +1,7 @@
 package ee.digit25.detector.domain.person;
 
 import ee.digit25.detector.domain.person.external.PersonRequester;
+import ee.digit25.detector.domain.person.external.api.PersonModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,25 +13,30 @@ public class PersonValidator {
 
     private final PersonRequester requester;
 
-    private boolean hasWarrantIssued(String personCode) {
-        log.info("Cheking if person ({}) has a warrant issued", personCode);
+    private boolean hasWarrantIssued(PersonModel person) {
+        log.info("Checking if person ({}) has a warrant issued", person.getPersonCode());
 
-        return requester.get(personCode).getWarrantIssued();
+        return person.getWarrantIssued();
     }
 
-    private boolean hasContract(String personCode) {
-        log.info("Cheking if person ({}) has a contract", personCode);
+    private boolean hasContract(PersonModel person) {
+        log.info("Checking if person ({}) has a contract", person.getPersonCode());
 
-        return requester.get(personCode).getHasContract();
+        return person.getHasContract();
     }
 
-    private boolean isBlacklisted(String personCode) {
-        log.info("Cheking if person ({}) is blacklisted", personCode);
+    private boolean isBlacklisted(PersonModel person) {
+        log.info("Checking if person ({}) is blacklisted", person.getPersonCode());
 
-        return requester.get(personCode).getBlacklisted();
+        return person.getBlacklisted();
+    }
+
+    private PersonModel getPerson(String personCode) {
+        return requester.get(personCode);
     }
 
     public boolean isValid(String personCode) {
-        return !hasWarrantIssued(personCode) && hasContract(personCode) && !isBlacklisted(personCode);
+        PersonModel person = getPerson(personCode);
+        return !hasWarrantIssued(person) && hasContract(person) && !isBlacklisted(person);
     }
 }
